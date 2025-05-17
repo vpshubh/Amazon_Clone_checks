c2e41350def4b9766311ef299cef5448d38ab429
vpshubh <chakravartyshubh23@gmail.com>
2025-05-17 13:36:03 +0530

    first commit

---
+++ core/urls.py
@@ -0,0 +1,8 @@
+# Amazon_Clone/urls.py
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('products.urls')),
+]
---
+++ products/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('products/', views.product_list, name='product_list'),
+    path('products/<int:product_id>/', views.product_detail, name='product_detail'),
+    path('cart/', views.cart, name='cart'),
+    path('checkout/', views.checkout, name='checkout'),
+    path('account/', views.account, name='account'),
+]
